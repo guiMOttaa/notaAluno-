ee7d8527deba383edd7902d8d9e6e32908039948
Guilherme Motta Nunes <124638368+guiMOttaa@users.noreply.github.com>
2024-04-15 19:30:57 -0300

    Add files via upload

---
+++ nota.py
@@ -0,0 +1,23 @@
+class aluno:
+    def __init__(self , nome,nota = 0) -> None:
+        self.nome = nome 
+        self.nota = nota
+
+        def faz_nota(self,qnt):
+         self.nota += qnt
+         
+
+         def verifica_nota( self ,media):
+          if self.verifica >=media :
+             print(f'{self.nota}Aprovado')
+          else:
+             print(f'{self.nota}Reprovado')
+
+
+        def mostrar(self):
+         print(f'o Aluno{self.nome}teve essa {self.nota}.')
+
+         pass
+         pass
+         pass
+        
